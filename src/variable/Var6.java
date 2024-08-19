f54d998e1bc1c0fa092b765960853ca28c7e99b6
thrkrif <103479629+thrkrif@users.noreply.github.com>
2024-08-20 00:35:25 +0900

    24/08/20

---
+++ src/variable/Var6.java
@@ -0,0 +1,11 @@
+package variable;
+
+public class Var6 {
+    public  static void main(String[] args){
+        char a = 'b';
+        String b = "Hello World!";
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(b);
+    }
+}
