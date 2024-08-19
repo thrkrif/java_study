49bd7964791d9933dbf5ab21bb81926a2c6d7835
thrkrif <103479629+thrkrif@users.noreply.github.com>
2024-08-19 23:44:26 +0900

    24/08/19 exam1

---
+++ src/variable/Var2.java
@@ -0,0 +1,12 @@
+package variable;
+
+public class Var2 {
+    public static void main(String[] args){
+        int a;
+        a = 10;
+        System.out.println(a);
+
+        int b = 20; // 변수 선언과 동시에 초기화
+        
+    }
+}
